2bc84a00782b002da2c2ff32bd891b1f8946e949
trenerit <piotrventuro@wp.pl>
2024-05-21 13:49:07 +0200

    add validate form

--- js/form.js
+++ js/form.js
@@ -40,20 +40,61 @@ const getDataFromSrv = async dataFromForm => {
 
 }
 
+const validateData = (e) => {
+    
+    e.preventDefault();
 
+    const resPlace = document.querySelector('#resultPlace');
+    resPlace.innerText = '';
+    resPlace.classList.remove('alert', 'alert-success');
+
+    labelAll.forEach((elem, i) => {
+
+        // console.log(elem, i);
+        elem.classList.remove('alert', 'alert-danger');
+        elem.innerText = labelVal[i];
+
+    });
+
+    const mail = document.querySelector('#mail').value;
+    const subject = document.querySelector('#title').value;
+    const message = document.querySelector('#message').value;
+
+    const dataFromSrv = {
+        mail,
+        subject,
+        message
+    }
+
+    getDataFromSrv(dataFromSrv)
+    .then(res => {
+        console.log(res);
+
+        if('send' in res) {
+            
+            resPlace.innerText = res.send;
+            resPlace.classList.add('alert', 'alert-success');
+            document.querySelectorAll('input:not(input[type="submit"]), textarea').forEach(elem => {
+                elem.value = '';
+            });
+
+        } else {
+            
+            if('email' in res) {
+                showError(labelAll[0], res.email);
+            }
+            
+            if('subject' in res) {
+                showError(labelAll[1], res.subject);
+            }
+            
+            if('message' in res) {
+                showError(labelAll[2], res.message);
+            }
+        }
+    });
+}
 
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-
-    e.preventDefault();
-
-    getDataFromSrv({
-        "mail": '',
-        "subject": '',
-        "message": ''
-    }).then(data => {
-        console.log(data);
-    });
-
-});
+form.addEventListener('submit', validateData);
